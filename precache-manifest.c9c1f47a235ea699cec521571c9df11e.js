26e837cf49447616a5640bb13ffe4ad17f62c5e5
Polly <polly.fisher@gmail.com>
2019-04-02 09:46:13 -0500

    Updates

---
+++ precache-manifest.c9c1f47a235ea699cec521571c9df11e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cfa64fa28c525a8a671",
+    "url": "/fun-food-friends/static/js/runtime~main.23ba6af9.js"
+  },
+  {
+    "revision": "8135b1c7ec94f2cf515e",
+    "url": "/fun-food-friends/static/js/main.80fd32d4.chunk.js"
+  },
+  {
+    "revision": "a7a51fbcd28f11a7de31",
+    "url": "/fun-food-friends/static/js/2.5d4e56f9.chunk.js"
+  },
+  {
+    "revision": "8135b1c7ec94f2cf515e",
+    "url": "/fun-food-friends/static/css/main.b625399a.chunk.css"
+  },
+  {
+    "revision": "b214438f2485c5bd73c3515146c9dbe9",
+    "url": "/fun-food-friends/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/fun-food-friends/precache-manifest.c9c1f47a235ea699cec521571c9df11e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fun-food-friends/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
